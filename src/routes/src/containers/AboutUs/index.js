c53c99aa2f70e33c4c2cca66ea90fe486ee93d43
Kamil Komarowski <k.w.komarowski@gmail.com>
2022-03-14 22:52:13 +0100

    Formating changes

--- src/routes/src/containers/AboutUs/index.js
+++ src/routes/src/containers/AboutUs/index.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { AboutCarousel, Crew, Section } from 'components';
 
 const AboutUs = () => (
-	<>
-		<Section title='O nas'>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugit
-				magni repellendus perspiciatis quis, eaque quae sunt veniam, voluptatem
-				voluptas sint saepe rem inventore beatae, optio iure aperiam placeat
-				illum! Maiores similique vero, provident est odit, quaerat consequatur
-				ad eveniet necessitatibus officiis molestiae sit quod dolorum cum,
-				veniam magnam aliquam dolore quisquam nulla consequuntur. Culpa eos
-				dolor aperiam repudiandae beatae? Iusto odit blanditiis id distinctio
-				iure ab, culpa officia, expedita, eos excepturi beatae in totam
-				sapiente! Expedita, ex esse odio nostrum fugiat debitis illum magnam,
-				culpa accusamus quae adipisci sunt.
-			</p>
-		</Section>
-		<AboutCarousel />
-		<Crew />
-	</>
+  <>
+    <Section title='O nas'>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugit
+        magni repellendus perspiciatis quis, eaque quae sunt veniam, voluptatem
+        voluptas sint saepe rem inventore beatae, optio iure aperiam placeat
+        illum! Maiores similique vero, provident est odit, quaerat consequatur
+        ad eveniet necessitatibus officiis molestiae sit quod dolorum cum,
+        veniam magnam aliquam dolore quisquam nulla consequuntur. Culpa eos
+        dolor aperiam repudiandae beatae? Iusto odit blanditiis id distinctio
+        iure ab, culpa officia, expedita, eos excepturi beatae in totam
+        sapiente! Expedita, ex esse odio nostrum fugiat debitis illum magnam,
+        culpa accusamus quae adipisci sunt.
+      </p>
+    </Section>
+    <AboutCarousel />
+    <Crew />
+  </>
 );
 
 export default AboutUs;
