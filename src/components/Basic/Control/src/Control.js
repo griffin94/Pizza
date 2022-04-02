deb330cef3c4b6861da94353fec2f9263f0f04fc
Kamil Komarowski <k.w.komarowski@gmail.com>
2022-04-02 19:50:31 +0200

    Fieldset and Control error handling

--- src/components/Basic/Control/src/Control.js
+++ src/components/Basic/Control/src/Control.js
@@ -4,66 +4,68 @@ import { cx } from 'utils';
 import cs from './Control.module.scss';
 
 const Control = ({
-	checked,
-	className,
-	id,
-	label,
-	name,
-	onChange,
-	type,
-	value,
+  checked,
+  className,
+  error,
+  id,
+  label,
+  name,
+  onChange,
+  type,
+  value,
 }) => {
-	const inputRef = useRef(null);
+  const inputRef = useRef(null);
 
-	const checkInput = (e) => {
-		e.preventDefault();
-		(e.code === 'Space' || e.code === 'Enter') &&
-			inputRef.current !== null &&
-			inputRef.current.click();
-	};
+  const checkInput = (e) => {
+    e.preventDefault();
+    (e.code === 'Space' || e.code === 'Enter') &&
+      inputRef.current !== null &&
+      inputRef.current.click();
+  };
 
-	return (
-		<div
-			className={cx(
-				className,
-				cs.container,
-				{
-					radio: cs.radio,
-					checkbox: cs.checkbox,
-				}[type],
-			)}
-		>
-			<input
-				checked={checked}
-				className={cx(cs.input)}
-				id={id}
-				name={name}
-				onChange={onChange}
-				ref={inputRef}
-				type={type}
-				value={value}
-			/>
-			<label
-				className={cx(cs.label)}
-				htmlFor={id}
-				onKeyPress={checkInput}
-				tabIndex='0'
-			>
-				{label}
-			</label>
-		</div>
-	);
+  return (
+    <div
+      className={cx(
+        className,
+        cs.container,
+        {
+          radio: cs.radio,
+          checkbox: cs.checkbox,
+        }[type],
+      )}
+    >
+      <input
+        checked={checked}
+        className={cx(cs.input)}
+        id={id}
+        name={name}
+        onChange={onChange}
+        ref={inputRef}
+        type={type}
+        value={value}
+      />
+      <label
+        className={cx(cs.label, error && cs.error)}
+        htmlFor={id}
+        onKeyPress={checkInput}
+        tabIndex='0'
+      >
+        {label}
+      </label>
+    </div>
+  );
 };
 
 Control.propTypes = {
-	checked: PropTypes.bool,
-	className: PropTypes.string,
-	id: PropTypes.string,
-	label: PropTypes.string,
-	name: PropTypes.string,
-	onChange: PropTypes.func,
-	type: PropTypes.string,
-	value: PropTypes.string,
+  checked: PropTypes.bool,
+  className: PropTypes.string,
+  error: PropTypes.bool,
+  id: PropTypes.string,
+  label: PropTypes.string,
+  name: PropTypes.string,
+  onChange: PropTypes.func,
+  type: PropTypes.string,
+  value: PropTypes.string,
 };
 
 export default Control;
--- src/components/Basic/Fieldset/index.js
+++ src/components/Basic/Fieldset/index.js
@@ -1,18 +1,27 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import cs from './src/Fieldset.module.scss';
 import { cx } from 'utils';
 
-const Fieldset = ({ className, children, legend }) => (
-	<fieldset className={cx(className, cs.container)}>
-		<legend className={cx(cs.legend)}>{legend}</legend>
-		{children}
-	</fieldset>
+const Fieldset = ({ className, children, error, legend }) => (
+  <fieldset className={cx(className, cs.container)}>
+    <legend className={cx(cs.legend)}>{legend}</legend>
+    <div className={cx(cs.inputs)}>
+      {React.Children.map(children, (child) =>
+        React.cloneElement(child, {
+          error: !!error,
+        }),
+      )}
+    </div>
+    <span className={cx(cs.errorContainer)}>{error}</span>
+  </fieldset>
 );
 
 Fieldset.propTypes = {
-	className: PropTypes.string,
-	children: PropTypes.node,
-	legend: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.node,
+  error: PropTypes.string,
+  legend: PropTypes.string,
 };
 
 export default Fieldset;
