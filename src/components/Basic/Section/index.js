1a5d611906e075254020afe29eace761be2cc299
Kamil Komarowski <k.w.komarowski@gmail.com>
2022-05-02 15:21:05 +0200

    Added possibility to pass custom className to component, added css property to capitalize title

--- src/components/Basic/Section/index.js
+++ src/components/Basic/Section/index.js
@@ -2,16 +2,17 @@ import PropTypes from 'prop-types';
 import cs from './src/Section.module.scss';
 import { cx } from 'utils';
 
-const Section = ({ children, title }) => (
-	<section className={cx(cs.section)}>
+const Section = ({ children, className, title }) => (
+	<section className={cx(cs.section, className)}>
 		{title && <h2 className={cx(cs.title)}>{title}</h2>}
 		{children}
 	</section>
 );
 
 Section.propTypes = {
-	title: PropTypes.string,
 	children: PropTypes.node,
+	className: PropTypes.string,
+	title: PropTypes.string,
 };
 
 export default Section;
