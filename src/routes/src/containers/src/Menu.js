f5fdf5a09f7cad9066a4bbf82c042ed54819c806
Kamil Komarowski <k.w.komarowski@gmail.com>
2022-06-16 21:35:30 +0200

    Added quick filters to menu view

--- src/routes/src/containers/src/Menu.js
+++ src/routes/src/containers/src/Menu.js
@@ -1,26 +1,42 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchMenuData } from 'redux/actions/creators';
 import { Menu } from '../../views';
 
-const MenuContainer =  () => {
+const MenuContainer = () => {
   const { data, error, loading } = useSelector((state) => state.menu);
+  const [activeFilter, setFilter] = useState(null);
+  const [visibleData, setVisibleData] = useState(data);
   const dispatch = useDispatch();
 
   useEffect(() => {
     !data && loadData();
   }, []);
 
+  useEffect(() => {
+    setVisibleData(
+      activeFilter ? { [activeFilter]: data[activeFilter] } : data,
+    );
+  }, [activeFilter, data]);
+
   const loadData = () => dispatch(fetchMenuData());
-  
+
+  const filter = (category) =>
+    setFilter((currentFilter) =>
+      currentFilter === category ? null : category,
+    );
+
   return (
     <Menu
+      activeFilter={activeFilter}
       data={data}
       error={error}
+      filter={filter}
       loadData={loadData}
       loading={loading}
+      visibleData={visibleData}
     />
-  )
+  );
 };
 
 export default MenuContainer;
--- src/routes/src/views/src/Menu/index.js
+++ src/routes/src/views/src/Menu/index.js
@@ -1,23 +1,42 @@
 import PropTypes from 'prop-types';
-import { Products } from 'components';
+import { Button, Products, Section } from 'components';
 import { withLoader } from 'components/HOC';
 import useMenu from './src/useMenu';
 import { CATEGORIES_DICTIONARY } from 'routes/src/constans';
+import { cx } from 'utils';
+import cs from './src/Menu.module.scss';
+import { KINDS } from 'components/Basic/Button/src/Button';
 
-const Menu = ({ data }) =>
-  (({ chooseProduct }) =>
-    Object.entries(data).map(([category, products]) => (
-      <Products category={CATEGORIES_DICTIONARY[category]} key={category}>
-        {products.map((product) => (
-          <Products.Item
-            key={product.id}
-            category={category}
-            onClick={chooseProduct}
-            {...product}
-          />
+const Menu = ({ activeFilter, data, filter, visibleData }) =>
+  (({ chooseProduct }) => (
+    <>
+      <Section className={cx(cs.filters)}>
+        {Object.keys(data).map((category) => (
+          <Button
+            className={cx(cs.button)}
+            kind={activeFilter === category ? KINDS.PRIMARY : null}
+            key={category}
+            onClick={() => filter(category)}
+          >
+            {CATEGORIES_DICTIONARY[category]}
+          </Button>
         ))}
-      </Products>
-    )))(useMenu());
+      </Section>
+      {visibleData &&
+        Object.entries(visibleData).map(([category, products]) => (
+          <Products category={CATEGORIES_DICTIONARY[category]} key={category}>
+            {products.map((product) => (
+              <Products.Item
+                key={product.id}
+                category={category}
+                onClick={chooseProduct}
+                {...product}
+              />
+            ))}
+          </Products>
+        ))}
+    </>
+  ))(useMenu());
 
 Menu.propTypes = {
   data: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.object)),
